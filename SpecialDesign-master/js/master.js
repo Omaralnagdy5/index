b4a257ed42035b8bc32402e363634fcc3855f9aa
Omaralnagdy5 <112776153+Omaralnagdy5@users.noreply.github.com>
2023-04-03 02:05:22 +0200

    Add files via upload

---
+++ SpecialDesign-master/js/master.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+let swappingCases = "elZERo";
+let invertedNumbers = [1, -10, -20, 15, 100, -30];
+let ignoreNumbers = "Elz123er4o";
+
+
+
+let sw = swappingCases
+.split("").map((a) =>(a === a.toLocaleLowerCase() ? a.toLocaleLowerCase() )) )
